92f9c1a608d27a36e601aa28a08e8bf6258e7bcb
tejpal singh <tej@dhwaniastro.com>
2026-05-15 14:38:18 +0530

    change for call

--- src/component/chat/Calling.js
+++ src/component/chat/Calling.js
@@ -164,7 +164,8 @@ const Calling = () => {
     });
 
     socket.on("ice-candidate", async (data) => {  
-       if (data.room_id === currentRequest?.room_id) {
+      
+       if (JSON.parse(data)?.room_id === currentRequest?.room_id) {
       try {
         if (peerConnectionRef.current && data.candidate) {
           await peerConnectionRef.current.addIceCandidate(
@@ -185,7 +186,7 @@ const Calling = () => {
     });
 
     socket.on("call_cancel_by_user", (data) => { 
-       if (data.roomId === currentRequest?.room_id) {
+       if (JSON.parse(data)?.room_id === currentRequest?.room_id) {
       cleanupCall();
     }
     });
