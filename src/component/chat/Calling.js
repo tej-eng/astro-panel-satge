e24363b1746189c56690144d1da6fbf640606413
tejpal singh <tej@dhwaniastro.com>
2026-05-15 13:09:24 +0530

    change for call

--- src/component/chat/Calling.js
+++ src/component/chat/Calling.js
@@ -64,7 +64,6 @@ const Calling = () => {
         },
       });
       localStreamRef.current = stream;
-      console.log("✅ Mic initialized on astrologer side");
     } catch (err) {
       console.error("❌ Mic access failed:", err);
     }
@@ -74,7 +73,6 @@ const Calling = () => {
   const createPeerConnection = (roomId) => {
     if (peerConnectionRef.current) return peerConnectionRef.current;
 
-    console.log("🟢 Creating PeerConnection (Astrologer)");
 
     const iceConfig = {
       iceServers: [
@@ -165,7 +163,8 @@ const Calling = () => {
       }
     });
 
-    socket.on("ice-candidate", async (data) => {
+    socket.on("ice-candidate", async (data) => {  
+       //if (data.room_id === currentRequest?.room_id) {
       try {
         if (peerConnectionRef.current && data.candidate) {
           await peerConnectionRef.current.addIceCandidate(
@@ -175,14 +174,19 @@ const Calling = () => {
       } catch (err) {
         console.error("❌ ICE Error:", err);
       }
+  //  }
     });
 
-    socket.on("call_ended_by_user", () => {
+    socket.on("call_ended_by_user", (data) => {
+     //if (data.room_id === currentRequest?.room_id) {
       cleanupCall();
+    //}
     });
 
-    socket.on("call_cancel_by_user", () => {
+    socket.on("call_cancel_by_user", (data) => { 
+      // if (data.roomId === currentRequest?.room_id) {
       cleanupCall();
+    //}
     });
 
     return () => {
@@ -220,6 +224,7 @@ const Calling = () => {
   const handleEndCall = () => {
     socket?.emit("call_ended_by_astrologer", {
       roomId: currentRequest?.room_id,
+      astroId: astroId,
     });
     cleanupCall();
   };
